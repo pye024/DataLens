9d6a665cf41f66a304f5baa578a44f8d01a6f257
pye024 <penieljegede@gmail.com>
2025-09-16 23:24:17 +0100

    Data Upload and Validation

--- app.py
+++ app.py
@@ -1,3 +1,20 @@
 import streamlit as st
-st.title("DataLens")
-st.write('DataLens')
+import pandas as pd
+#Title
+st.title("DataLens!🔎")
+st.write('')
+st.divider()
+
+
+
+#File Upload
+data = st.file_uploader(label="Upload a csv file")
+df=pd.read_csv(data)
+
+
+#Validation
+if df.empty == True or len(df.columns) == 0:
+    st.error("The Dataframe is empty or the columns dont have names")
+else:
+    st.success("Upload complete. Preview the data and continue to insights.")
+    st.dataframe(df.head(10))
